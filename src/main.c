e33d7f957ae5d22b59a9f155ac02b018675c5c1a
Ezequiel Ghiena <ezequielghra97@gmail.com>
2024-09-02 01:04:02 -0300

    Added forward declarations for functions

--- src/main.c
+++ src/main.c
@@ -21,69 +21,12 @@ typedef struct {
 
 typedef enum {false, true} bool;
 
-bool is_button_hovered(Button* button_ptr, int x_coord, int y_coord){
-    return (x_coord > button_ptr->rect.x && x_coord < button_ptr->rect.x + button_ptr->rect.w &&
-            y_coord > button_ptr->rect.y && y_coord < button_ptr->rect.y + button_ptr->rect.h);
-}
 
-
-void launch_program(const char* command, const char* button_label){
-    printf("Attempting to launch %s\n", button_label);
-    int status = system(command);
-    if (status == -1) fprintf(stderr, "Error launching program: %s\n", command);
-}
-
-
+bool is_button_hovered(Button* button_ptr, int x_coord, int y_coord);
+void launch_program(const char* command, const char* button_label);
 void draw_buttons_and_labels(Button* buttons_array, int num_buttons, TTF_Font* font,
-                             SDL_Renderer* renderer, int mouse_x, int mouse_y){
-    for (int i=0; i<num_buttons; i++){
-        Button btn = buttons_array[i];
-
-        // Check if the mouse is hovering the button
-        bool is_hovered = is_button_hovered(&btn, mouse_x, mouse_y);
-
-        // Draw button border
-        SDL_Rect border_rect = {btn.rect.x - BUTTON_BORDER_PX, btn.rect.y - BUTTON_BORDER_PX,
-                                btn.rect.w + 2*BUTTON_BORDER_PX, btn.rect.h + 2*BUTTON_BORDER_PX};
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-        SDL_RenderFillRect(renderer, &border_rect);
-
-        // Draw button
-        if (is_hovered)
-            SDL_SetRenderDrawColor(renderer, btn.hover_red, btn.hover_green, btn.hover_blue, btn.hover_alpha);
-        else
-            SDL_SetRenderDrawColor(renderer, btn.red, btn.green, btn.blue, btn.alpha);
-
-        SDL_RenderFillRect(renderer, &btn.rect);
-
-        // Render label
-        SDL_Color text_color = {btn.text_red, btn.text_green, btn.text_blue, btn.text_alpha};
-        SDL_Surface* text_surface = TTF_RenderText_Solid(font, btn.label, text_color);
-        SDL_Texture* text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
-
-        // Get text width and height
-        int text_width = 0, text_height = 0;
-        SDL_QueryTexture(text_texture, NULL, NULL, &text_width, &text_height);
-
-        // Calculate position to center text on button
-        int text_x_coord = btn.rect.x + (btn.rect.w - text_width)/2;
-        int text_y_coord = btn.rect.y + (btn.rect.h - text_height)/2;
-
-        SDL_Rect text_rect = {text_x_coord, text_y_coord, text_width, text_height};
-        SDL_RenderCopy(renderer, text_texture, NULL, &text_rect);
-
-        SDL_DestroyTexture(text_texture);
-        SDL_FreeSurface(text_surface);
-    }
-
-    SDL_RenderPresent(renderer);
-}
-
-
-void arrange_buttons(Button* buttons_array, int num_buttons){
-    printf("Arranging buttons\n");
-    for (int i=0; i<num_buttons; i++) buttons_array[i].rect.y = (2*i+1) * BUTTON_HEIGTH;
-}
+                             SDL_Renderer* renderer, int mouse_x, int mouse_y);
+void arrange_buttons(Button* buttons_array, int num_buttons);
 
 
 int main(int argc, char** argv){
@@ -158,4 +101,69 @@ int main(int argc, char** argv){
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+
+bool is_button_hovered(Button* button_ptr, int x_coord, int y_coord){
+    return (x_coord > button_ptr->rect.x && x_coord < button_ptr->rect.x + button_ptr->rect.w &&
+            y_coord > button_ptr->rect.y && y_coord < button_ptr->rect.y + button_ptr->rect.h);
+}
+
+
+void launch_program(const char* command, const char* button_label){
+    printf("Attempting to launch %s\n", button_label);
+    int status = system(command);
+    if (status == -1) fprintf(stderr, "Error launching program: %s\n", command);
+}
+
+
+void draw_buttons_and_labels(Button* buttons_array, int num_buttons, TTF_Font* font,
+                             SDL_Renderer* renderer, int mouse_x, int mouse_y){
+    for (int i=0; i<num_buttons; i++){
+        Button btn = buttons_array[i];
+
+        // Check if the mouse is hovering the button
+        bool is_hovered = is_button_hovered(&btn, mouse_x, mouse_y);
+
+        // Draw button border
+        SDL_Rect border_rect = {btn.rect.x - BUTTON_BORDER_PX, btn.rect.y - BUTTON_BORDER_PX,
+                                btn.rect.w + 2*BUTTON_BORDER_PX, btn.rect.h + 2*BUTTON_BORDER_PX};
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(renderer, &border_rect);
+
+        // Draw button
+        if (is_hovered)
+            SDL_SetRenderDrawColor(renderer, btn.hover_red, btn.hover_green, btn.hover_blue, btn.hover_alpha);
+        else
+            SDL_SetRenderDrawColor(renderer, btn.red, btn.green, btn.blue, btn.alpha);
+
+        SDL_RenderFillRect(renderer, &btn.rect);
+
+        // Render label
+        SDL_Color text_color = {btn.text_red, btn.text_green, btn.text_blue, btn.text_alpha};
+        SDL_Surface* text_surface = TTF_RenderText_Solid(font, btn.label, text_color);
+        SDL_Texture* text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+
+        // Get text width and height
+        int text_width = 0, text_height = 0;
+        SDL_QueryTexture(text_texture, NULL, NULL, &text_width, &text_height);
+
+        // Calculate position to center text on button
+        int text_x_coord = btn.rect.x + (btn.rect.w - text_width)/2;
+        int text_y_coord = btn.rect.y + (btn.rect.h - text_height)/2;
+
+        SDL_Rect text_rect = {text_x_coord, text_y_coord, text_width, text_height};
+        SDL_RenderCopy(renderer, text_texture, NULL, &text_rect);
+
+        SDL_DestroyTexture(text_texture);
+        SDL_FreeSurface(text_surface);
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
+
+void arrange_buttons(Button* buttons_array, int num_buttons){
+    printf("Arranging buttons\n");
+    for (int i=0; i<num_buttons; i++) buttons_array[i].rect.y = (2*i+1) * BUTTON_HEIGTH;
 }
